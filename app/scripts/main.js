1b326a1dc9aac37340df0e5b8c31cfbbc7af2c5f
ernest3001 <ernest3001@gmail.com>
2017-07-20 13:11:26 +0200

    Ok

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('\'Plugify \'Plugify!');
+
+
+$(document).ready(function(){
+
+var button = $(".buttonmsg");
+
+button.on("click", function(){
+    alert("Please put your e-mail below");
+})
+
+});
