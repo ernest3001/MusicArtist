71bfac14f41f6e04b11d24dbcb00cb39ca10ba29
ernest3001 <ernest3001@gmail.com>
2017-07-20 15:47:39 +0200

    Reasons to book and mobile nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,31 @@
 console.log('\'Plugify \'Plugify!');
 
-
+//jquery message for user to put email
 $(document).ready(function(){
 
 var button = $(".buttonmsg");
 
 button.on("click", function(){
-
     alert("Please confirm by clicking 'ok' and put your e-mail address below.")
 })
+
+
+// pure JavaScript function for mobile navigation menu
+function showMenu(){
+    var navigation = document.querySelector(".navigation");
+    var button = document.querySelector(".menu_btn");
+
+    button.addEventListener("click", function(event){
+      if(navigation.className.indexOf("hidden_menu") >=0){
+        navigation.classList.remove("hidden_menu");
+      }
+      else{
+        navigation.classList.add("hidden_menu");
+      }
+    })
+  }
+
+  showMenu();
 
 
 });
